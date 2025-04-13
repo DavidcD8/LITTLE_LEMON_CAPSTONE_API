4a2bd783f237397d36951d131587ec8fdf216dc2
DavidcD8 <davidcd08@gmail.com>
2025-04-13 12:04:01 +0100

    modify view for delivery to add delete and  post in one single class. Add manager delete function to managergroup view

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 from . import views
-from .views import AssignDeliveryView, AssignGroupView, RemoveDeliveryView, RemoveGroupView
+from .views import AssignDeliveryView, ManagerGroupView
 
 urlpatterns = [
     path('menu-items/', views.MenuItemListView.as_view()),
     path('menu-items/<int:pk>/', views.SingleMenuItemView.as_view()),
     path('create-category/', views.CreateCategory.as_view()),
     path('categories/', views.CategoryListView.as_view()),
-    path('users/group/', AssignGroupView.as_view(), name='assign-group'),
-    path('users/group/remove/', RemoveGroupView.as_view(), name='remove-group'),
-    path('users/group/delivery/', AssignDeliveryView.as_view(), name='assign-delivery'),
-    path('users/group/delivery/remove/', RemoveDeliveryView.as_view(), name='remove-delivery'),
+    path("groups/manager/users", ManagerGroupView.as_view(), name="add-manager"),
+    path("groups/manager/users/<int:user_id>", ManagerGroupView.as_view(), name="remove-manager"),
+    path('groups/delivery-crew/users/', AssignDeliveryView.as_view(), name='assign-delivery'),
+    path('groups/delivery-crew/users/<int:user_id>', AssignDeliveryView.as_view(), name='assign-delivery'),
 
 ]
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
 from .models import Category, MenuItem, Cart, Order, OrderItem
 from rest_framework import generics
 from rest_framework.views import APIView, Response, status
@@ -124,6 +124,18 @@ class SingleMenuItemView(APIView,DenyUnsafeMethodsMixin):
         return Response({"detail": "You do not have permission to perform this action."}, status=status.HTTP_403_FORBIDDEN)
 
 
+    def delete(self, request, pk): # this method deleted an object from menu
+        if IsManager().has_permission(request, self) or IsAdminUser().has_permission(request, self):
+            try:
+                item = MenuItem.objects.get(pk=pk)
+            except MenuItem.DoesNotExist:
+                return Response({"message": "Item not found."}, status=status.HTTP_404_NOT_FOUND)
+
+            item.delete()
+            return Response({"message": "Item deleted successfully."}, status=status.HTTP_200_OK)
+        return Response({"detail": "You do not have permission to perform this action."}, status=status.HTTP_403_FORBIDDEN)
+
+
 
 # View to create categories
 class CreateCategory(APIView):
@@ -151,91 +163,124 @@ class CategoryListView(APIView):
 
 
 # view to add user to Managers group
-class AssignGroupView(APIView):
-    permission_classes = [IsAdminUser]  # Checks if the user is an admin
+class ManagerGroupView(APIView):
+    permission_classes = [IsAdminUser | IsManager]
+
+    def get(self, request):
+        try:
+            # Get the Managers group
+            manager_group = Group.objects.get(name="Managers")
+        except Group.DoesNotExist:
+            return Response({"message": "Managers group does not exist"}, status=status.HTTP_404_NOT_FOUND)
+
+        # Get all users in the 'Managers' group
+        managers = manager_group.user_set.all()
+
+        # Create a list of users with the data you want to return
+        manager_data = [{"id": user.id, "username": user.username} for user in managers]
+
+        return Response({'managers': manager_data})
+
 
     def post(self, request):
+        user_id = request.data.get("user_id") # Gets the user ID from the request payload
+        if not user_id:
+            return Response({"detail": "Missing 'user_id' in payload."}, status=status.HTTP_400_BAD_REQUEST)
+
         try:
-            # Get the 'Managers' group from your database
-            manager_group = Group.objects.get(name='Managers')
-        except ObjectDoesNotExist:
-            # If the group doesn't exist, return a 404 error
-            return Response({'message': "Managers group does not exist"}, status=status.HTTP_404_NOT_FOUND)
+            user = User.objects.get(id=user_id) # Gets the user ID from the request payload
+            manager_group = Group.objects.get(name="Managers") # Retrieves the Group object with the name Managers from the database
 
-        # Check if the user is already in the 'Managers' group
-        if request.user.groups.filter(name='Managers').exists():
-            return Response({'message': "User is already in the managers group"}, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response({"detail": "User not found."}, status=status.HTTP_404_NOT_FOUND)
+        except Group.DoesNotExist:
+            return Response({"detail": "Managers group does not exist."}, status=status.HTTP_404_NOT_FOUND)
 
-        # Add the user to the 'Managers' group if they are not already in it
-        request.user.groups.add(manager_group)
-        return Response({'message': "User added to managers group"}, status=status.HTTP_201_CREATED)
+        if user.groups.filter(name="Managers").exists(): # Check if the user is in the Managers group
+            return Response({"message": "User is already in the Managers group."}, status=status.HTTP_200_OK)
+
+        user.groups.add(manager_group)
+        return Response({"message": "User added to Managers group."}, status=status.HTTP_201_CREATED)
 
 
+    def delete(self, request, user_id=None):
+        if not user_id:
+            return Response({"detail": "Missing user_id in URL."}, status=status.HTTP_400_BAD_REQUEST)
 
-# view to remove user from Managers group
-class RemoveGroupView(APIView):
-    permission_classes = [IsAdminUser]  # Checks if the user is an admin
-
-    def delete(self, request):
         try:
-            # Get the 'Managers' group
-            manager_group = Group.objects.get(name='Managers')
-        except ObjectDoesNotExist:
-            return Response({'message': "Managers group does not exist"}, status=status.HTTP_404_NOT_FOUND)
+            user = User.objects.get(id=user_id) # Gets the user ID from the request payload
+            manager_group = Group.objects.get(name="Managers") # Retrieves the Group object with the name Managers from the database
 
-        # Check if the user is in the 'Managers' group
-        if request.user.groups.filter(name='Managers').exists():
-            # Remove the user from the 'Managers' group
-            request.user.groups.remove(manager_group)
-            return Response({'message': "User removed from managers group"}, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response({"detail": "User not found."}, status=status.HTTP_404_NOT_FOUND)
+        except Group.DoesNotExist:
+            return Response({"detail": "Managers group does not exist."}, status=status.HTTP_404_NOT_FOUND)
 
-        # If the user is not in the 'Managers' group
-        return Response({'message': "User not found in the managers group"}, status=status.HTTP_404_NOT_FOUND)
+        if user.groups.filter(name="Managers").exists(): # Check if the user is in the Managers group
+            user.groups.remove(manager_group)
+            return Response({"message": "User removed from Managers group."}, status=status.HTTP_200_OK)
 
-
+        return Response({"message": "User not in Managers group."}, status=status.HTTP_404_NOT_FOUND)
 
 
 
-# view to add user to delivery group by manager
+
+
+
+
+# view to add user to delivery group by manager or admin
 class AssignDeliveryView(APIView):
-    permission_classes = [IsManager] # Checks if the user is an manager
+    permission_classes = [IsAdminUser | IsManager]
+
+    def get(self, request):
+        try:
+            delivery_group = Group.objects.get(name="Delivery Crew") # Get the Delivery group
+        except Group.DoesNotExist:
+            return Response({"message": "Delivery Crew does not exist"}, status=404)
+
+        # Get all users in the Delivery group
+        delivery = delivery_group.user_set.all()
+
+        # Create a list of users with the data you want to return
+        delivery_data = [{"id": user.id, "username": user.username} for user in delivery]
+
+        return Response({'delivery': delivery_data})
+
 
     def post(self, request):
+        user_id = request.data.get("user_id") # Gets the user ID from the request payload
+        if not user_id:
+            return Response({"detail": "Missing 'user_id' in payload."}, status=status.HTTP_400_BAD_REQUEST)
+
         try:
-            # Get the 'Delivery Crew' group
-            delivery_group = Group.objects.get(name='Delivery Crew')
-        except ObjectDoesNotExist:
-            return Response({'message': "Delivery group does not exist"}, status=status.HTTP_404_NOT_FOUND)
+            user = User.objects.get(id=user_id) # Gets the user ID from the request payload
+            delivery_group = Group.objects.get(name="Delivery Crew") # Retrieves the Group object with the name Delivery Crew from the database
 
-        # Check if the user is already in the 'Delivery Crew' group
-        if request.user.groups.filter(name='Delivery Crew').exists():
-            return Response({'message': "User is already in the Delivery Crew group"}, status=status.HTTP_400_BAD_REQUEST)
+        except User.DoesNotExist:
+            return Response({"detail": "User not found."}, status=status.HTTP_404_NOT_FOUND)
+        except Group.DoesNotExist:
+            return Response({"detail": "Delivery group does not exist."}, status=status.HTTP_404_NOT_FOUND)
 
-        # Add the user to the 'Delivery Crew' group
-        request.user.groups.add(delivery_group)
+        if user.groups.filter(name="Delivery Crew").exists(): # Check if the user is in the Delivery group
+            return Response({"message": "User is already in the Delivery Crew."}, status=status.HTTP_200_OK)
 
-        # Return a success response
-        return Response({'message': "User successfully added to the Delivery Crew group"}, status=status.HTTP_200_OK)
-
+        user.groups.add(delivery_group)
+        return Response({"message": "User added to Delivery group."}, status=status.HTTP_201_CREATED)
 
 
-# view to remove user from delivery group
-class RemoveDeliveryView(APIView):
-    permission_classes = [IsManager]  # Checks if the user is an manager
-
-    def delete(self, request):
+    def delete(self, request, user_id=None):
+        if not user_id:
+            return Response({"detail": "Missing user_id in URL."}, status=status.HTTP_400_BAD_REQUEST)
         try:
-            # Get the 'Delivery' group
-            delivery_group = Group.objects.get(name='Delivery Crew')
-        except ObjectDoesNotExist:
-            return Response({'message': "Delivery group does not exist"}, status=status.HTTP_404_NOT_FOUND)
+            user = User.objects.get(id=user_id) # Gets the user ID from the request payload
+            delivery_group = Group.objects.get(name="Delivery Crew") # Retrieves the Group object with the name Delivery Crew from the database
 
-        # Check if the user is in the 'Delivery' group
-        if request.user.groups.filter(name='Delivery Crew').exists():
-            # Remove the user from the 'Delivery' group
-            request.user.groups.remove(delivery_group)
-            return Response({'message': "User removed from Delivery group"}, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+                return Response({"detail": "User not found."}, status=status.HTTP_404_NOT_FOUND)
+        except Group.DoesNotExist:
+            return Response({"detail": "Delivery Crew does not exist."}, status=status.HTTP_404_NOT_FOUND)
+        if user.groups.filter(name="Delivery Crew").exists(): # Checks if the user is in the Delivery Crew
+            user.groups.remove(delivery_group)
+            return Response({"message": "User removed from Delivery Crew."}, status=status.HTTP_200_OK)
 
-        # If the user is not in the 'Delivery' group
-        return Response({'message': "User not found in the Delivery group"}, status=status.HTTP_404_NOT_FOUND)
-
+        return Response({"message": "User not in Delivery Crew."}, status=status.HTTP_404_NOT_FOUND)
